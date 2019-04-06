bdb88ed28c238102a6dda530a6418162952d55e0
Gabriel K <gabrielkukiel@gmail.com>
2019-04-05 20:56:50 -0400

    Correção dos horarios na store

--- src/store/ducks/settings.js
+++ src/store/ducks/settings.js
@@ -1,6 +1,6 @@
 const initialState = [{
     minutoAula: 45,
-    timeStamps: ["07:00","07:45","08:30","09:30","10:15","10:45","11:30","12:15","13:00","13:45","14:30","15:30","16:15","17:00","17:45","18:30"],
+    timeStamps: ["07:00","07:45","08:30","09:30","10:15","11:00","11:45","12:30","13:00","13:45","14:30","15:30","16:15","17:00","17:45","18:30"],
     dias: ["Domingo","Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"]
 }]
 export default function turmas(state = initialState){
