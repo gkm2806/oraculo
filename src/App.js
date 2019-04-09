2c5190adfb43e9c52a1abbc638db6cbd704c1ae4
Gabriel K <gabrielkukiel@gmail.com>
2019-04-09 14:50:51 -0400

    Loading global e inicio da manipulaçao dos routes params

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 import {Route, Switch} from 'react-router-dom'
 import { connect } from 'react-redux';
 import { withRouter } from "react-router-dom";
-
+import {Spin} from "antd"
 import MenuNav from "./components/menu/Menu"
 import Schedule from "./components/schedule/Schedule"
 import Manager from "./components/manager/Manager"
@@ -16,24 +16,36 @@ import Auth from "./utils/Auth.js"
 class App extends Component {
 
   render() {
-    const {user} = this.props
+    const {user,globalLoading} = this.props
     return (
       <div className="App">
         <MenuNav name="Oraculo" ></MenuNav>
         <main>
+            {globalLoading ? (
+              <center><Spin style={{marginTop:"20%"}} spinning={globalLoading} delay={500}></Spin></center>
+            ):(
           <Switch>
               <Route 
+                component={(match) => <Schedule type="salas" match={match} ctx={this.props.salas} />}
+                path="/salas/:search" 
+              />
+              <Route 
+                component={(match) => <Schedule type="turmas" match={match} ctx={this.props.turmas} />}
+                path="/turmas/:search" 
+              />
+               <Route 
                 component={() => <Schedule type="salas" ctx={this.props.salas} />}
-                exact path="/salas" 
+                path="/salas/" 
               />
               <Route 
                 component={() => <Schedule type="turmas" ctx={this.props.turmas} />}
-                exact path="/turmas" 
+                path="/turmas/" 
               />
               <Route component={Auth(Manager,user,1,NoAcess)} exact path="/manager" ></Route>
               <Route component={Auth(Aula,user,1,NoAcess)} exact path="/aula" ></Route>
               <Route component={UserLogin} exact path="/login" ></Route>
           </Switch>
+          )}
         </main>
       </div>
     );
@@ -43,7 +55,8 @@ class App extends Component {
 const mapStateToProps = (state) => ({
   salas: state.salas,
   turmas: state.turmas,
-  user: state.user
+  user: state.user,
+  globalLoading: state.settings.globalLoading
 })
 
 export default withRouter(connect(mapStateToProps)(App));
