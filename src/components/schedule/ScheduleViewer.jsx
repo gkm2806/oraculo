7f5667e6a8396b3c1e569650fd4a1c6e89caa966
Gabriel K <gabrielkukiel@gmail.com>
2019-04-12 18:21:43 -0400

    Adicionando Scroll Container

--- src/components/schedule/ScheduleViewer.jsx
+++ src/components/schedule/ScheduleViewer.jsx
@@ -1,7 +1,7 @@
 
 import React, { Component } from "react";
-import {Row, Col, Card} from "antd"
-
+import { Row, Col, Card } from "antd"
+import InfiniteScroll from 'react-infinite-scroller';
 import Dia from "./Dia"
 
 export default class ScheduleViewer extends Component {
@@ -9,31 +9,35 @@ export default class ScheduleViewer extends Component {
 
     }
     render() {
-        const {timeStamps, aulas, search, type} = this.props
+        const { timeStamps, aulas, search, type } = this.props
         return (
-            (search ?(
-            <div id="alo">
-                <div>
+            (search ? (
+                <div style={scrollable}>
                     <Col span={3} >
                         <Row><header> Hora </header></Row>
-                        {timeStamps.map((time)=>{
-                            return( <Row  key={time} ><Card style={{backgroundColor: "#999", padding:0}}>{time}</Card></Row>)
+                        {timeStamps.map((time) => {
+                            return (<Row key={time} ><Card style={{ backgroundColor: "#999", padding: 0 }}>{time}</Card></Row>)
                         })}
                     </Col>
 
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Domingo" key="Domingo" aulas={aulas.filter(aula=> aula.dia === "Domingo")}/>
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Segunda" key="Segunda" aulas={aulas.filter(aula=> aula.dia === "Segunda")}/>
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Terça"   key="Terça"   aulas={aulas.filter(aula=> aula.dia === "Terça")}/>
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Quarta"  key="Quarta"  aulas={aulas.filter(aula=> aula.dia === "Quarta")}/>
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Quinta"  key="Quinta"  aulas={aulas.filter(aula=> aula.dia === "Quinta")}/>
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Sexta"   key="Sexta"   aulas={aulas.filter(aula=> aula.dia === "Sexta")}/>
-                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Sabado"  key="Sabado"  aulas={aulas.filter(aula=> aula.dia === "Sabado")}/>
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Domingo" key="Domingo" aulas={aulas.filter(aula => aula.dia === "Domingo")} />
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Segunda" key="Segunda" aulas={aulas.filter(aula => aula.dia === "Segunda")} />
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Terça" key="Terça" aulas={aulas.filter(aula => aula.dia === "Terça")} />
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Quarta" key="Quarta" aulas={aulas.filter(aula => aula.dia === "Quarta")} />
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Quinta" key="Quinta" aulas={aulas.filter(aula => aula.dia === "Quinta")} />
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Sexta" key="Sexta" aulas={aulas.filter(aula => aula.dia === "Sexta")} />
+                    <Dia type={type} search={search} timeStamps={timeStamps} dNome="Sabado" key="Sabado" aulas={aulas.filter(aula => aula.dia === "Sabado")} />
                 </div>
 
-            </div>
-            ):(
-                <h2> Selecione um contexto no menu ao lado</h2>
-            ))
+            ) : (
+                    <h2> Selecione um contexto no menu ao lado</h2>
+                )
+            )
         )
     }
+}
+
+const scrollable = {
+    overflowY: "scroll",
+    height: "40em"
 }
