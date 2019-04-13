5aae0f5945ea89e1dd61c98374d1369c4cf6cf29
Gabriel K <gabrielkukiel@gmail.com>
2019-04-13 00:22:45 -0400

    Adaptação do Schedule para o novo sistema de pesquisa global

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
 import { Col } from "antd"
-
-import ScheduleMenu from "./ScheduleMenu"
-import ScheduleViewer from "./ScheduleViewer"
+import {bindActionCreators} from "redux"
+import {Creators as SearchActions } from "../../store/ducks/search"
+import capitalizer from "../../utils/capitalizer"
+import ScheduleMenu from "./menu/ScheduleMenu"
+import ScheduleViewer from "./viewer/ScheduleViewer"
 
 class Schedule extends Component {
   state = {
     searched: ""
   }
+  componentDidMount(){
+    const { match} = this.props 
+
+    match && (
+      this.setState({searched: capitalizer(match.match.params.search)})
+    )
+  }
 
   setSearched = (query) => {
-    this.setState({ searched: query })
-    console.log(this.props.aulas.filter(aula => Object.values(aula).indexOf(this.state.searched)))
+    this.props.updateSearch(query)
   }
 
   render() {
-    const { ctx, settings, aulas, type } = this.props
+    const { ctx, settings, aulas, type,search } = this.props
     return (
 
       <div style={{ width: "100%" }}>
@@ -26,7 +34,7 @@ class Schedule extends Component {
               <ScheduleMenu type={type} setSearched={this.setSearched} ctx={ctx} />
             </Col>
             <Col span={20} style={{ marginLeft: "1rem" }}>
-              <ScheduleViewer type={type} search={this.state.searched} aulas={aulas.filter(aula => (Object.values(aula)).includes(this.state.searched))} timeStamps={settings[0].timeStamps} />
+              <ScheduleViewer type={type} search={search.searched} aulas={aulas.filter(aula => (Object.values(aula)).includes(search.searched))} timeStamps={settings.timeStamps} />
             </Col>
           </div>
         }
@@ -35,10 +43,16 @@ class Schedule extends Component {
     )
   }
 }
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state,props) => ({
   salas: state.salas,
   turmas: state.turmas,
   settings: state.settings,
-  aulas: state.aulas.aulas
+  aulas: state.aulas.aulas,
+  search: state.search,
+  match: props.match
 })
-export default connect(mapStateToProps)(Schedule)
+const mapDispatchToProps = dispatch => bindActionCreators({
+  ...SearchActions
+}, dispatch)
+
+export default connect(mapStateToProps,mapDispatchToProps)(Schedule)
