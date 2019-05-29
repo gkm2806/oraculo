1179eb179c448433652234a7b747f7304aacd241
Gabriel K <gabrielkukiel@gmail.com>
2019-05-28 20:26:58 -0400

    atualizando o sistema de login com suap

--- src/components/user/UserLogin.jsx
+++ src/components/user/UserLogin.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"
-import {Form, Icon, Input, Button, Col, notification} from 'antd';
+import { Form, Icon, Input, Button, Col, notification, Switch } from 'antd';
 import axios from "axios"
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom"
 import "dotenv/config"
 import { bindActionCreators } from "redux"
 import { Creators as userActions } from "../../store/ducks/user";
 //import history from "../../utils/history"
 
 class UserLogin extends Component {
+    state = {
+        isSuap: false
+    }
     openNotification = (type, message = null) => {
         switch (type) {
             case "info":
@@ -34,7 +37,7 @@ class UserLogin extends Component {
         }
 
     };
-    sucessNotification = () =>{
+    sucessNotification = () => {
         notification["sucess"]({
             message: "Foi",
             description: "Usuario Conectado",
@@ -44,26 +47,47 @@ class UserLogin extends Component {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log("username: ", values.userName, "\n senha: ", values.password);
-                axios.post(
-                    `${'http://201.2.48.240:443'}/api/users/login`,
-                        {
-                            username: values.userName,
-                            password: values.password
-                        }
-                    )
-                    .then((response) => {
-                        this.props.loginUser({
-                            userName: response.data.username,
-                            permission: response.data.permission,
-                            token: response.data.token
+                {this.state.isSuap 
+                    ? (
+                        axios.post(
+                            `https://suap.ifms.edu.br/api/autenticacao/token/`,
+                            {
+                                username: values.userName,
+                                password: values.password
+                            }
+                        )
+                        .then((response) => {
+                            this.props.loginUser({
+                                userName: response.data.username,
+                                permission: response.data.permission,
+                                token: response.data.token
+                            })
+                            console.log("response: ", response)
+                            this.props.history.push('/manager')
+                        }).catch((err) => {
+                            console.log("erro", err.response.data.message)
+                            this.openNotification("error", err.response.data.message)
                         })
-                        console.log("response: ", response)
-                        this.props.history.push('/manager')
-                    }).catch((err) => {
-                        console.log("erro", err.response.data.message)
-                        this.openNotification("error", err.response.data.message)
-                    })
+                    ):(
+                        axios.post(
+                            `${'http://shaolinapi.ddns.net:443'}/api/users/login`,
+                            {
+                                username: values.userName,
+                                password: values.password
+                            }
+                        )
+                        .then((response) => {
+                            this.props.loginUser({
+                                ...response.data,
+                                permission: 1
+                            })
+                            console.log("response: ", response)
+                            this.props.history.push('/manager')
+                        }).catch((err) => {
+                            console.log("erro", err.response.data)
+                            this.openNotification("error", err.response.data.message)
+                        })
+                )}
             }
         });
     }
@@ -72,31 +96,33 @@ class UserLogin extends Component {
     }
     render() {
         const { getFieldDecorator } = this.props.form;
+        const { isSuap } = this.state
         return (
-            <div style={{width:"100%", display:"flex"}}>
-            <Col>
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                        {getFieldDecorator('userName', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
-                        })(
-                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                    </Button>
-                    </Form.Item>
-                </Form>
-            </Col>
+            <div style={{ width: "100%", display: "flex" }}>
+                <Col>
+                    <Switch onChange={()=> this.setState({isSuap : !isSuap})} checkedChildren="suap" unCheckedChildren="ifms" />
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                        </Button>
+                        </Form.Item>
+                    </Form>
+                </Col>
             </div>
         );
     }
